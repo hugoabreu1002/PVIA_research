4e42e44e97112b4fcb43e695f60df7bbcf6ef230
Hugo Abreu Mendes <hugoabreu1002@gmail.com>
2021-07-23 00:19:53 -0300

    simulate function

--- BESSSim/SimpleBESS.py
+++ BESSSim/SimpleBESS.py
@@ -6,3 +6,9 @@ class SimpleBEES(object):
 
     def simulate(self, R_true, R_pred, Charge_data, Initial_Cond=1):
         
+        Charge_data = Charge_data/max(Charge_data)
+        R_true = R_true/max(R_true)
+        R_pred = R_pred/max(R_pred)
+
+        power_flux_pred = Initial_Cond + self._eFi*R_pred - Charge_data
+        power_flux_true = Initial_Cond + self._eFi*R_true - Charge_data
