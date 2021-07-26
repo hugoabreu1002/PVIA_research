612987ffb21f1cc0689951dc45f3fadce7618893
Hugo Abreu Mendes <hugoabreu1002@gmail.com>
2021-07-26 00:34:47 -0300

    simulation

--- BESSSim/SimpleBESS.py
+++ BESSSim/SimpleBESS.py
@@ -1,14 +1,66 @@
+import pandas as pd
+import numpy as np
+
 class SimpleBEES(object):
-    def __init__(self, eFi, backUpPercent) -> None:
+    def __init__(self, CostAndDemandFile="./BESSSim/Cost_and_hourly_demand.xlsx") -> None:
         super().__init__()
-        self._eFi = eFi
-        self._backUpPercent = backUpPercent
+        self._CostAndDemandFile = CostAndDemandFile
 
-    def simulate(self, R_true, R_pred, Charge_data, Initial_Cond=1):
+    def simulate(self, R_true, R_pred, solarRadeFi=0.15, DemandData=None, DemandFactor=0.3, State_of_Chage_I=0.5, State_of_Chage_F = 0.5):
+        """
+            Charge_data: The data of the charge on the BESS, if None, uses the data loaded by the CostAndDemandFile.
+            Default = None.
+
+            returns power_flux_true, power_flux_pred
+        """
+        if DemandData == None:
+            self.loadCostAndDemandCurves(DemandFactor)
+        else:
+            self._DemandData = DemandData
         
-        Charge_data = Charge_data/max(Charge_data)
-        R_true = R_true/max(R_true)
-        R_pred = R_pred/max(R_pred)
+        R_true = R_true
+        R_pred = R_pred
 
-        power_flux_pred = Initial_Cond + self._eFi*R_pred - Charge_data
-        power_flux_true = Initial_Cond + self._eFi*R_true - Charge_data
+        power_generated_pred = solarRadeFi*R_pred
+        power_generated_true = solarRadeFi*R_true
+
+        batery_charge_pred = np.zeros(len(self._DemandData))
+        batery_charge_true = np.zeros(len(self._DemandData))
+        batery_charge_pred[0] = State_of_Chage_I
+        batery_charge_true[0] = State_of_Chage_I
+        
+        for i in range(1, len(self._DemandData)):
+
+            batery_charge_pred[i] = batery_charge_pred[i-1] + power_generated_pred[i] - self._DemandData[i]
+            batery_charge_true[i] = batery_charge_true[i-1] + power_generated_true[i] - self._DemandData[i]
+
+            if batery_charge_pred[i] < 0:
+                batery_charge_pred[i] = 0
+                
+            if batery_charge_true[i] < 0:
+                batery_charge_true[i] = 0
+
+            if batery_charge_pred[i] > 1 :
+                batery_charge_pred[i] = 1
+                
+            if batery_charge_true[i] > 1:
+                batery_charge_true[i] = 1
+            
+        return batery_charge_true, batery_charge_pred
+
+    def syncDemand(self):
+        return None    
+
+    def loadCostAndDemandCurves(self, DemandFactor):
+        df = pd.read_excel(self._CostAndDemandFile, engine="openpyxl")
+        df_p = self.resampleDF(df)
+        self._DemandData = df_p["Demand Variation [%]"].values
+        self._DemandData = self._DemandData/100*DemandFactor
+        self._CoEpu = df_p["CoE [pu]"].values
+
+    def resampleDF(self, df, resample_rate=2, offset=0):
+        df_new = {}
+        for column in df.columns:
+            df_new[column] = df[column].values[0::resample_rate]
+
+        return pd.DataFrame(df_new)
--- BESSSim/readme.md
+++ BESSSim/readme.md
@@ -1,3 +1,17 @@
 # Purpose
 
 Simulate the Optimal Dispatch Problem for BESS.
+
+Cost and Hourly Demand Data in the file [Cost_and_hourly_demand.xlsx](Cost_and_hourly_demand.xlsx) was took from [1].
+
+# Possible Scenarios for BESS.
+
+From [1].
+
+* Scenario 1 (S1): This scenario analyzes the optimal dispatch of DCMG considering that the BESSs begin and end their daily operation in the fully discharged state.
+* Scenario 2 (S2): In this scenario is considered that the BESS must begin and end their daily operation with a 50% charge. However, during their daily operation, they can discharge or charge from 0% to 100% of their nominal capacity.
+* Scenario 3 (S3): In this scenario is analyzed that the BESS can only vary their nominal capacity from 50% to 100% during the day.
+
+# REFERENCES
+
+1. Gil-González, Walter, et al. "Economic dispatch of renewable generators and BESS in DC microgrids using second-order cone optimization." Energies 13.7 (2020): 1703.
